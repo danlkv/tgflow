b3fed34ce630a44c5dcf44cab427abb59895bb97
DaniloZZZ <lkv97dn@gmail.com>
2018-04-01 16:32:06 +0300

    import fixes and first example

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
       name = 'tgflow',
       packages = ['tgflow'],
-      version = '0.1',
+      version = '0.1.4rc',
       description = 'A declarative-style telegram bot framework',
       author = 'Danil Lykov',
       author_email = 'lkvdan@gmail.com',
       url = 'https://github.com/DaniloZZZ/tgflow',
       download_url = 'https://github.com/DaniloZZZ/tgflow/archive/0.1.tar.gz',
-      #install_requires=['telebot','enum'],
-      #python_requires='>=3.3',
+      install_requires=['telebot'],
+      python_requires='>=3.3',
       license='MIT',
       keywords = ['tools', 'telegram', 'framework', 'bot'], # arbitrary keywords
       classifiers = [],
