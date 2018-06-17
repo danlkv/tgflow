af87a33438c591e851bb02ebc86cf918730450e4
Danilozzz <lkv97dn@gmail.com>
2018-06-18 00:55:20 +0300

    added universal api for any bot platform

---
+++ tgflow/api/base.py
@@ -0,0 +1,17 @@
+class tgfAPI:
+
+    def __init__(self,token):
+        self.token=token
+    def start(self):
+        raise NotImplemlemntedError()
+    def send(self):
+        raise NotImplemlemntedError()
+    def update(self):
+        raise NotImplemlemntedError()
+    def set_callback_handler(self):
+        raise NotImplemlemntedError()
+    def set_message_handler(self):
+        raise NotImplemlemntedError()
+
+
+
---
+++ tgflow/api/tg.py
@@ -0,0 +1,46 @@
+from .base import tgfAPI
+import telebot
+
+class telegramAPI(tgfAPI):
+    def __init__(self,token):
+        super().__init__(token)
+        self.bot = telebot.TeleBot(token)
+
+    def start(self,**args):
+        self.bot.polling(**args)
+
+    def send(self,id,**args):
+        self.bot.send_message(
+            text=args['text'],
+            chat_id=id,
+            parse_mode='Markdown',
+            reply_markup =args['markup']
+        )
+
+    def update(self,msg,**args):
+        # here msg is the type set_message_handler pass
+        text =args.get('text')
+        markup=args.get('markup')
+        if text:
+            self.bot.edit_message_text(
+                text=text,
+                chat_id=msg.chat.id,
+                parse_mode='Markdown',
+                message_id=msg.message_id,
+            )
+        if markup:
+            print("tgflow: updating markup",markup)
+            self.bot.edit_message_reply_markup(
+                chat_id=msg.chat.id,
+                message_id=msg.message_id,
+                reply_markup=markup)
+
+    def set_message_handler(self,clb):
+        self.bot.set_update_listener(clb)
+
+    def set_callback_handler(self,clb):
+        # is there analog to upd_listener in telebot?
+        @self.bot.callback_query_handler(func=lambda x:True)
+        def stub(call):
+            clb(call)
+
