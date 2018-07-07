69c8be7e8ea6de305a099b6d53e4a120288dc349
Danilozzz <lkv97dn@gmail.com>
2018-07-08 01:07:59 +0300

    added example for vk

---
+++ examples/vk.py
@@ -0,0 +1,76 @@
+# This is needed to import module in this folder
+import sys
+sys.path.insert(0, '..')
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from tgflow import TgFlow as tgf
+from tgflow import handles as h
+from tgflow.api.vk import vkAPI
+from enum import Enum
+key='API_KEY'
+group='GROUP_ID'
+
+
+class States(Enum):
+    START=1
+    INFO=2
+    FAV=3
+    THANKS=4
+
+ui = {
+    States.START:{
+        't':'Hello there!',
+        'b':[
+         {'show my info':tgf.action(States.INFO)},
+        ]
+    },
+    States.INFO:{
+        't':'Your fav is s',
+        'b': [
+            {'back to start':tgf.action(States.START)},
+        ]
+    },
+}
+tgf.configure(token=key,
+              state=States.START,
+              apiModel=vkAPI,
+              data={"foo":'bar'},
+              group=group
+             )
+tgf.start(ui)
+
+
+"""
+UI = {
+    States.START:
+    {'t':'Hello, User!\n Here\'s what you can do:',
+     'b': [
+         {'show my info':tgf.action(States.INFO)},
+         {'set my favourite':h.action(States.FAV)}
+     ]},
+    States.FAV:{
+        't':'Please send me name of your favourite thing',
+        'b':[{'my favourite is star':(lambda input,data: (States.THANKS,{'fav':'star'}))}],
+        'react':h.action(
+            lambda i: (States.THANKS,{'fav':i.text}),
+            react_to = 'text'),
+    },
+    States.INFO:{
+        't':h.st('Your fav is %s','fav'),
+        'b': [
+            {'back to start':tgf.action(States.START)},
+            {'overwrite favourite':h.action(States.FAV)}
+        ]
+    },
+    States.THANKS:{
+        't':h.st('Thanks! I will remember your %s','fav'),
+         'b': [
+             {'show info':h.action(States.INFO,update_msg=True)},
+             {'set another favourite':h.action(States.FAV,update_msg=True)}
+         ],
+        'clear_trig':'text'},
+    }
+
+"""
